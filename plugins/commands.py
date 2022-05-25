97b930471c9b7541d4c4b6c7645d8eb335b64aad
Alice <101930427+AliceWabot@users.noreply.github.com>
2022-05-25 17:01:19 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -273,5 +273,5 @@ async def about(client, message):
                 ]                
             ]
         ),
-        reply_to_message_id=message.chat.id
+        reply_to_message_id=message.message_id
     )
